7b9a5b71474521135130e86ee70b7fd1e4488625
Maikel Mardjan <mike@bm-support.org>
2025-06-12 16:41:47 +0200

    header fix

--- src/ultrafastrss/ultrafastrss.py
+++ src/ultrafastrss/ultrafastrss.py
@@ -30,6 +30,8 @@ nocxheaders = {
     "user-agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:139.0) Gecko/20100101 Firefox/139.0",
     "Accept": "text/html, application/xhtml+xml, application/xml;q=0.9, */*;q=0.8",
     "Accept-Encoding": "gzip, deflate,br",
+    "Connection": "keep-alive",  
+    "Upgrade-Insecure-Requests": "1",  
 }
 nocxtimeout = 7
 
